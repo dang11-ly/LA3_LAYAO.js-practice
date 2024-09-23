0eb12c02d6897e3e071dd00028d49531ad184962
dang11-ly <layaoamandayvelle@gmail.com>
2024-09-23 17:50:22 +0800

    Add files via upload

---
+++ booleans.js.js
@@ -0,0 +1,9 @@
+// Boolenas
+var isJsProgrammingLanguange = true;
+var isJsHard = false;
+var favNum = 11;
+favNumb = undefined;
+
+console.log("isJsProgrammingLanguange:", isJsProgrammingLanguange);
+console.log("isJsHard:", isJsHard);
+console.log("FavNumb:", favNumb);
